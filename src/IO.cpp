3f7122f6cf41450818de891efb9fee8139c53e6b
aklt <at92001@gmail.com>
2021-09-11 17:30:44 -0400

    Move templated functions to headers

--- src/IO.cpp
+++ src/IO.cpp
@@ -49,18 +49,6 @@ std::vector<std::string> File::read_lines() {
     return lines;
 }
 
-template <class T>
-T File::read() {
-    T x;
-    file >> x;
-    return x;
-}
-
-template <class T>
-void File::write(const T& x) {
-    file << x;
-}
-
 void File::write_line(const std::string& line) {
     file << line << std::endl;
 }
@@ -71,50 +59,3 @@ void File::write_lines(const std::vector<std::string>& lines) {
     }
 }
 
-// Reads a string and returns the elements in the string
-template <class T>
-std::vector<T> read_vector(const std::string& line) {
-    std::vector<T> v;
-    std::stringstream ss(line);
-    while (!ss.eof()) {
-        T x;
-        ss >> x;
-        v.push_back(x);
-    }
-    return v;
-}
-
-// Writes matrix to file
-template <class T>
-void File::write_matrix(const Matrix<T,Dynamic,Dynamic>& m) {
-    file << m;
-    file.flush();
-}
-
-// Reads matrix from file. Also checks that it's a rectangular matrix.
-// Note: Every line must have exactly the same number of elements (including the final line!)
-// So can't have newline by itself.
-template <class T>
-Matrix<T,Dynamic,Dynamic> File::read_matrix() {
-    rewind();
-    std::vector<T> data; // Holds elements of matrix
-    int cols = -1;
-    int rows = 0;
-    while (!eof()) {
-        std::string line = read_line();
-        rows++;
-        auto v = read_vector<T>(line);
-        if (cols == -1) {
-            // How many columns on this line
-            cols = v.size();
-        } else if (v.size() != cols) {
-            // Uneven number of columns
-            throw MatrixReadingError();
-        }
-        // Append to the data read so far
-        std::copy(v.begin(), v.end(), std::back_inserter(data));
-    }
-    // Return in matrix format
-    Map<Matrix<T,Dynamic,Dynamic,RowMajor>> m(&data[0], rows, cols);
-    return m;
-}
